78959e41fc02f3c9c0434b34a6d1a84b26c06937
B1ZON-c0de <roman.andreicko@icloud.com>
2025-06-17 01:16:26 +0700

    Add files via upload

---
+++ 12-this/this-animal/script.js
@@ -0,0 +1,25 @@
+const dog = {
+  name: 'Чарли',
+  type: 'Собака',
+  makeSound() {
+    return 'Гав-Гав';
+  },
+};
+
+const bird = {
+  name: 'Петя',
+  type: 'Воробей',
+  makeSound() {
+    return 'Чик-чирик'; // ЧЫК-ЧЫРЫК xD
+  },
+};
+
+function makeDomestic(isDomestic) {
+  this.isDomestic = isDomestic;
+  console.log(`${this.type} по имени ${this.name} говорит ${this.makeSound()}`);
+  return this;
+}
+
+makeDomestic.bind(dog, true)();
+makeDomestic.call(bird, false);
+makeDomestic.apply(bird, [false]);
