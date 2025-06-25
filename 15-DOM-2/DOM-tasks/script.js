26c06c6b758a554c898b04693383f95cc67b8f11
B1ZON-c0de <roman.andreicko@icloud.com>
2025-06-25 19:48:12 +0700

    Update script.js

--- 15-DOM-2/DOM-tasks/script.js
+++ 15-DOM-2/DOM-tasks/script.js
@@ -1,25 +1,11 @@
 const body = document.querySelector('body');
-// Toggle Dark Theme -------
-document.addEventListener('keydown', (e) => {
-  if (e.key === 'Tab') {
-    e.preventDefault();
-    body.classList.toggle('dark-theme');
-  }
-});
-
-//----------
-
 const tasksList = document.querySelector('.tasks-list');
-
 const inputTask = document.querySelector('.create-task-block__input');
 const submit = document.querySelector('.create-task-block');
 
+// Инициализация блока ошибки
 const errorBlock = document.createElement('span');
 errorBlock.className = 'error-message-block';
-
-createModal();
-const modal = document.querySelector('.modal-overlay');
-let deleteModalBtn = null;
 
 const tasks = [
   {
@@ -39,17 +25,39 @@ const tasks = [
   },
 ];
 
+// Переменная для хранения кнпоки удаления выбранной задачи
+let deleteModalBtn = null;
+
+// Переключение тем (светлая-тёмная)
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'Tab') {
+    e.preventDefault();
+    body.classList.toggle('dark-theme');
+  }
+});
+
+// Создание и инициализация модального окна
+createModal();
+const modal = document.querySelector('.modal-overlay');
+
+// Инициализация задач и обработчика к ним
+renderTasks(tasks, tasksList);
+initHandlersTasks();
+
+// Отображение задач
 function renderTasks(arrTasks, tasksList) {
   tasksList.innerHTML = '';
 
-  arrTasks.forEach(({ id, text }) =>
+  arrTasks.forEach(({ id, text, completed }) =>
     tasksList.insertAdjacentHTML(
       'beforeend',
       `<div class="task-item" data-task-id="${id}">
     <div class="task-item__main-container">
         <div class="task-item__main-content">
             <form class="checkbox-form">
-                <input class="checkbox-form__checkbox" type="checkbox" id="task-${id}">
+                <input class="checkbox-form__checkbox" type="checkbox" id="task-${id}" ${
+        completed ? 'checked' : ''
+      }>
                 <label for="task-${id}"></label>
             </form>
             <span class="task-item__text">
@@ -64,17 +72,7 @@ function renderTasks(arrTasks, tasksList) {
     )
   );
 }
-function completeTask(e) {
-  const textSpans = document.querySelectorAll('.task-item__text');
-  const task = tasks.find((el) => el.id === e.target.id.replace('task-', ''));
-  const taskText = [...textSpans].find(
-    (el) => el.closest('.task-item').dataset.taskId === task.id
-  );
-  task.completed = e.target.checked;
-  task.completed
-    ? (taskText.style.textDecoration = 'line-through')
-    : (taskText.style.textDecoration = '');
-}
+// Обработка создания задачи
 function handlerSubmit(e) {
   e.preventDefault();
   if (
@@ -94,27 +92,37 @@ function handlerSubmit(e) {
     });
     inputTask.value = '';
     renderTasks(tasks, tasksList);
-    handleTasks();
   }
 }
-function deleteTask(e) {
+// Обработка кнопок задачи (удаление и выполнение)
+function handleBtnsTask(e) {
   const deleteBtnTask = e.target.closest('.task-item__delete-button');
+  const completeBtn = e.target.closest('.checkbox-form__checkbox');
 
   if (deleteBtnTask) {
-    const modal = document.querySelector('.modal-overlay');
     modal.classList.remove('modal-overlay_hidden');
 
     deleteModalBtn = deleteBtnTask;
   }
+  if (completeBtn) {
+    const textSpans = document.querySelectorAll('.task-item__text');
+    const task = tasks.find((el) => el.id === e.target.id.replace('task-', ''));
+    const taskText = [...textSpans].find(
+      (el) => el.closest('.task-item').dataset.taskId === task.id
+    );
+    task.completed = e.target.checked;
+    task.completed
+      ? (taskText.style.textDecoration = 'line-through')
+      : (taskText.style.textDecoration = '');
+  }
 }
-function handleTasks() {
-  const completeBtns = document.querySelectorAll('.checkbox-form__checkbox');
-
+// Глобальные обработчики задач
+function initHandlersTasks() {
   submit.addEventListener('submit', handlerSubmit);
-  completeBtns.forEach((el) => el.addEventListener('click', completeTask));
-  tasksList.addEventListener('click', deleteTask);
+  tasksList.addEventListener('click', handleBtnsTask);
 }
 
+// Создание модального окна удаления в DOM
 function createModal() {
   body.insertAdjacentHTML(
     'beforeend',
@@ -136,6 +144,8 @@ function createModal() {
   );
   handleModal();
 }
+
+// Логика обработчика кнопок моадльного окна удаления(ОТМЕНА И УДАЛИТЬ)
 function handleModal() {
   const cancelBtn = document.querySelector('.delete-modal__cancel-button');
   const confirmBtn = document.querySelector('.delete-modal__confirm-button');
@@ -150,11 +160,6 @@ function handleConfirmButton() {
     tasks.find((el) => el.id === task.dataset.taskId)
   );
   tasks.splice(indexTask, 1);
-  console.log(tasks);
   modal.classList.add('modal-overlay_hidden');
   renderTasks(tasks, tasksList);
-  handleTasks();
 }
-
-renderTasks(tasks, tasksList);
-handleTasks();
